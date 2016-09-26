326b783652fed5a23db46071fa0362caafe89c60
shabakka <nwolfegwow@gmail.com>
2016-09-26 18:52:02 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -2,70 +2,49 @@ import json
 import math
 
 def load_data(filepath):
-    with open(filepath, encoding='UTF-8') as file:
-        return json.load(file)
-    pass
+    with open(filepath, encoding='UTF-8') as loadeddata:
+        return json.load(loadeddata)
 
 
 def get_biggest_bar(data):
     maxname = ''
-    lst = load_data(data)
-    maxelem = lst[0]['Cells']['SeatsCount']
-    for element in lst:
+    loadeddata = load_data(data)
+    maxelem = loadeddata[0]['Cells']['SeatsCount']
+    for element in loadeddata:
         if maxelem < element['Cells']['SeatsCount']:
             maxelem = element['Cells']['SeatsCount']
             maxname = element['Cells']['Name']
     print('Biggest Bar is "' + maxname + '", which seats ' + str(maxelem) + ' people.\n')
-    pass
 
 
 def get_smallest_bar(data):
     minname = ''
-    lst = load_data(data)
-    minelem = lst[0]['Cells']['SeatsCount']
-    for element in lst:
+    loadeddata = load_data(data)
+    minelem = loadeddata[0]['Cells']['SeatsCount']
+    for element in loadeddata:
         if minelem > element['Cells']['SeatsCount']:
             minelem = element['Cells']['SeatsCount']
             minname = element['Cells']['Name']
     print('Smallest bar is "' + minname + '", which seats ' + str(minelem) + ' people.')
-    pass
 
 
 def get_closest_bar(data, longitude, latitude):
     closestbar = ''
     checkpair = []
     result = 0.0
-    lst = load_data(data)
+    loadeddata = load_data(data)
     checkpair.append(longitude)
     checkpair.append(latitude)
-    for element in lst:
+    for element in loadeddata:
         tempcoordpair = element['Cells']['geoData']['coordinates']
         if result < math.sqrt((tempcoordpair[0] - checkpair[0]) ** 2 + (tempcoordpair[1] - checkpair[1]) ** 2):
             result = math.sqrt((tempcoordpair[0] - checkpair[0]) ** 2 + (tempcoordpair[1] - checkpair[1]) ** 2)
             closestbar = 'Closest bar to your location is "' + element['Cells']['Name'] + '".'
     return closestbar
-    pass
-
-def maxmin(data):
-    lst = load_data(data)
-    maxname = ''
-    minname = ''
-    minelem = lst[0]['Cells']['SeatsCount']
-    maxelem = lst[0]['Cells']['SeatsCount']
-    for element in lst:
-        if maxelem < element['Cells']['SeatsCount']:
-            maxelem = element['Cells']['SeatsCount']
-            maxname = element['Cells']['Name']
-        elif minelem > element['Cells']['SeatsCount']:
-            minelem = element['Cells']['SeatsCount']
-            minname = element['Cells']['Name']
-    print('Biggest Bar is "' + maxname + '", which seats ' + str(
-        maxelem) + ' people.\n' + 'Smallest is "' + minname + '", which seats ' + str(minelem) + ' people.')
 
 
 if __name__ == '__main__':
     path = 'D:/test/Бары.json'
-    #print(maxmin(path))
     print(get_biggest_bar(path))
     print(get_smallest_bar(path))
     while 1:
